966c657b3e491e847fc3a485171e4f2e4c73fbb5
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-05-08 20:38:03 +0200

    chore: bump deps for v6.0.0-beta.2 release

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package fileserver provides an HTTP middleware plugin for RoadRunner that serves
+// static files from the filesystem with support for ETags, directory listings,
+// and configurable cache control headers.
+package fileserver
